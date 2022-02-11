f52cc9ff8a314704f6045d6137dd43f1f6858701
algumaaaa <matheus_sampson@hotmail.com>
2022-02-11 19:23:50 -0300

    ammoSpawn integrated into r equation
    changed ammo to ammoSpawn

--- ammopickup.gd
+++ ammopickup.gd
@@ -1,7 +1,9 @@
 extends Spatial
 
+onready var GLOBAL = get_node("/root/GLOBAL/")
 export var ammotype = 0
 onready var sprite = $Sprite3D
+var ammoplus = 0
 
 func _ready():
 	sprite.frame = ammotype
@@ -10,13 +12,29 @@ func _ready():
 func _on_Area_body_entered(body):
 	if body.is_in_group("player"):
 		if ammotype == 0:
-			body.nelevenammo += 20
-			body.mac10ammo += 30
+			body.nelevenammo += 30 + ammoplus
+			body.mac10ammo += 30 + ammoplus
 		if ammotype == 1:
-			body.doublebammo += 10
+			body.doublebammo += 20 + floor((ammoplus * 0.5))
 		if ammotype == 2:
-			body.glauncherammo += 4
+			body.glauncherammo += 4 + floor((ammoplus * 0.1))
 		if ammotype == 3:
-			body.lactionammo += 7
+			body.lactionammo += 7 + floor((ammoplus * 0.3))
 		queue_free()
 	pass 
+
+func _on_door_body_entered(body):
+	if body.is_in_group("player"):
+		pass
+
+func _manage_ammo():
+	var p = self.get_parent()
+	for c in p.get_children():
+		if GLOBAL.r > 150:
+			c.ammoplus = 50
+		if GLOBAL.r >= 101 and GLOBAL.r <= 150:
+			c.ammoplus = 40
+		if GLOBAL.r >= 34 and GLOBAL.r <= 100:
+			c.ammoplus = 30
+		if GLOBAL.r >= 33:
+			c.ammoplus = 20
