621f27558317e379e768b2c929fdcf174254ae07
algumaaaa <matheus_sampson@hotmail.com>
2022-02-13 22:20:55 -0300

    More sound work

--- bloodpuddle.gd
+++ bloodpuddle.gd
@@ -1,12 +1,20 @@
 extends Spatial
 
 var onfloor = false
+var bloodSounds = preload("res://Audio/Gore/Drop_Blood_02.wav")
 
+onready var bloodPlayer = $AudioStreamPlayer3D
 onready var sprite3d = $Sprite3D
 onready var animplay = $AnimationPlayer
 
 func _ready():
+	randomize()
+
 	if onfloor:
+		bloodPlayer.stream = bloodSounds
+		var rand = rand_range(0, 1)
+		if rand > 0.5:
+			bloodPlayer.play()
 		sprite3d.frame_coords = Vector2(5, 0)
 	else:
 		sprite3d.frame_coords = Vector2(4, 0)
